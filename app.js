c78bc9af83b90dfd3a1f3d63e8808a32e94bebc2
hzyuxiaohua <hzyuxiaohua@corp.netease.com>
2014-06-04 23:32:38 +0800

    develop m1

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// develop m1
 var app = express();
 <<<<<<< HEAD
 <<<<<<< HEAD
