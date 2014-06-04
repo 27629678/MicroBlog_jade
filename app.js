e610a6be687c562b3924eab8c9fb68e63ca0ddaf
hzyuxiaohua <hzyuxiaohua@corp.netease.com>
2014-06-04 23:10:53 +0800

    master m1

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+<<<<<<< HEAD
 // m1
 // m2
+=======
+// master m1
+>>>>>>> master m1
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
